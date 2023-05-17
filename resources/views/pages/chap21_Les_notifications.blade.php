554fe87af274e33a5f5ee1bd5ecf5bb7b5a9b342
Gaetach21 <tachgaetan@gmail.com>
2023-05-17 21:00:56 +0100

    Add chapitre21:Les notifications

--- resources/views/pages/chap21_Les_notifications.blade.php
+++ resources/views/pages/chap21_Les_notifications.blade.php
@@ -15,54 +15,34 @@ L'utilisateur va recevoir le message suivant dans UserRegisteredNotification.php
 <img src="{{ asset('images/laravel_chap21_image2.jpg') }}"><br>
 On enregistre un nouvel utilisateur en cliquant sur le lien inscription.<br>
 Il nous retourne le tableau de bord et on se rend ensuite sur mailtrap.io.<br>
-<img src="{{ asset('images/laravel_chap21_image3.jpg') }}" style="height: 300px;"><br>
-<!-- On crée un compte sous mailtrap.io pour recevoir nos mails de test.<br>
-<img src="{{ asset('images/laravel_chap20_image4.jpg') }}"> -->
+<img src="{{ asset('images/laravel_chap21_image3.jpg') }}" style="height: 250px;"><br>
 </p>
 
-<!-- <h3>Envoi d'un mail dynamique</h3>
+<!-- <h3>Envoi d'une notifications avec informations</h3>
 <p>
-Pour envoyer un mail dynamique, on crée un faux utilisateur qui a pour email : 
-<strong>'user@test.com'</strong> et pour nom : <strong>monsieur Gaetan</strong>.<br>
-Dans TestController.php<br>
-<img src="{{ asset('images/laravel_chap20_image5.jpg') }}"><br>
-Dans App/Mail/TestMail.php<br>
-<img src="{{ asset('images/laravel_chap20_image6.jpg') }}"><br>
-Dans resources/views/emails/test.blade.php<br>
-<img src="{{ asset('images/laravel_chap20_image7.jpg') }}"><br>
+Maintenant on veut passer des petites informations à la construction de notre notification.<br>
+Dans RegisteredUserController.php<br>
+<img src="{{ asset('images/laravel_chap21_image4.jpg') }}"><br>
+Dans UserRegisteredNotification.php<br>
+<img src="{{ asset('images/laravel_chap21_image5.jpg') }}"><br>
 Sous mailtrap.io<br>
-<img src="{{ asset('images/laravel_chap20_image8.jpg') }}">
+<img src="{{ asset('images/laravel_chap21_image6.jpg') }}">
 </p>
 
-<h3>Envoi d'un mail avec pièce jointe</h3>
+<h3>Envoi d'une notification avec mail</h3>
+on est libre de créer le mail que l'on veut.<br>
+<strong>php artisan make:mail TestNotificationMail</strong><br>
 <p>
-Dans App/Mail/TestMail.php<br>
-<img src="{{ asset('images/laravel_chap20_image9.jpg') }}"><br>
+Dans TestNotificationMail.php<br>
+<img src="{{ asset('images/laravel_chap21_image7.jpg') }}"><br>
+dans test-notification-mail.php<br>
+<img src="{{ asset('images/laravel_chap21_image8.jpg') }}"><br>
+Il est question d'envoyer un mailable depuis la notification.
+Dans UserRegisteredNotification.php
+<img src="{{ asset('images/laravel_chap21_image9.jpg') }}"><br>
 Sous mailtrap.io<br>
-<img src="{{ asset('images/laravel_chap20_image10.jpg') }}"><br>
-On peut utiliser la méthode attachFromStorage qui appelle le dossier storage/app<br>
-<img src="{{ asset('images/laravel_chap20_image11.jpg') }}">
-</p>
-
-<h3>Envoi d'un mail avec markdown</h3>
-<p>
-Sur la console : <strong>php artisan make:mail TestMarkdownMail -m emails.markdown-test</strong><br>
-Dans resources/views/emails, on crée un fichier markdown-test.blade.php<br>
-<img src="{{ asset('images/laravel_chap20_image12.jpg') }}"><br>
-Dans App\Mail, on a le fichier TestMarkdownMail.php<br>
-<img src="{{ asset('images/laravel_chap20_image13.jpg') }}"><br>
-dans le fichier .env : <br>
-<strong>APP_NAME=GaetanCuisine</strong><br>
-<strong>MAIL_FROM_ADDRESS=tachgaetan@gmail.com</strong><br>
-Dans TestController.php<br>
-<img src="{{ asset('images/laravel_chap20_image14.jpg') }}"><br>
-Sous mailtrap.io<br>
-<img src="{{ asset('images/laravel_chap20_image15.jpg') }}">
+<img src="{{ asset('images/laravel_chap21_image10.jpg') }}"><br>
 </p> -->
-
-
-
-
 
 <div>
     <a href="{{ route('chap20') }}" class="bg-primary p-2 text-white" style="text-decoration: none;">Chapitre20: Envoi des mails</a>
